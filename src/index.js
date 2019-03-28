94eb64be680b7a8b6d3861662e38c823797c632d
OscarFabiani <spartan539@yahoo.com>
2019-03-28 15:29:32 -0700

    Added Display componnet and win logic.

--- src/index.js
+++ src/index.js
@@ -9,22 +9,22 @@ class GameBoard extends React.Component {
     this.state = {
       turn: 'x',
       winner: '',
-      tracker: {
-        a: '',
-        b: '',
-        c: '',
-        d: '',
-        e: '',
-        f: '',
-        g: '',
-        h: '',
-      }
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      e: '',
+      f: '',
+      g: '',
+      h: '',
+      i: '',
     }
-    this.switchTurn = this.switchTurn.bind(this);
+    this.toggleTurn = this.toggleTurn.bind(this);
     this.checkForWin = this.checkForWin.bind(this);
+    this.updateTracker = this.updateTracker.bind(this);
+    this.reset = this.reset.bind(this);
   }
-  switchTurn() {
-    console.log(true, this.state.turn);
+  toggleTurn() {
     if(this.state.turn === 'x') {
       this.setState({
         turn: 'o',
@@ -37,23 +37,61 @@ class GameBoard extends React.Component {
     }
   }
   checkForWin() {
-    let tracker = this.state.tracker;
-    if(tracker.a != '' && tracker.a === tracker.b && tracker.a === tracker.c) {
-      console.log(true);
+    let[a, b, c, d, e, f, g, h, i] = [
+      this.state.a, this.state.b, this.state.c, this.state.d, this.state.e, this.state.f, this.state.g, this.state.h, this.state.i
+    ];
+    if(
+      (a !== '' && ((a === b && a === c) || (a === d && a === g) || (a === e && a === i)))
+        || (c !== '' && ((c === f && c === i) || (c === e && c === g)))
+          || (d !== '' && (d === e && d === f))
+            || (g !== '' && (g === h && g === i))
+              || (b !== '' && (b === e && b === h))) {
+      this.setState({
+        winner: this.state.turn === 'x' ? 'O' : 'X',
+      })
     }
   }
+  updateTracker(id, turn) {
+    this.setState({
+      [id]: turn,
+      didReset: false,
+    })
+  }
+  reset() {
+    this.setState({
+      turn: 'x',
+      winner: '',
+      a: '',
+      b: '',
+      c: '',
+      d: '',
+      e: '',
+      f: '',
+      g: '',
+      h: '',
+      i: '',
+    })
+  }
   render() {
+    let spaces = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'].map(v =>
+      <Space 
+        key={v}
+        id={v}
+        turn={this.state.turn}
+        winner={this.state.winner}
+        letter={this.state[v]}
+        updateTracker={this.updateTracker}
+        toggleTurn={this.toggleTurn}
+        checkForWin={this.checkForWin}/>);
     return (
-      <div className='game-board'>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
-        <Space turn={this.state.turn} tracker ={this.state.tracker} switchTurn={this.switchTurn} checkForWin={this.checkForWin}/>
+      <div>
+        <div className='game-board'>
+        {spaces}
+        </div>
+        <Display
+          turn={this.state.turn}
+          winner={this.state.winner}
+          reset={this.reset}/>
       </div>
     )
   }
@@ -63,31 +101,33 @@ class Space extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showMe: false,
+      visible: false,
     }
     this.handleClick = this.handleClick.bind(this);
   }
-  //Maybe add a method that update the tracker somewhere.
   handleClick() {
-    this.props.checkForWin();
-    this.props.switchTurn();
+    this.props.updateTracker(this.props.id, this.props.turn);
+    this.props.toggleTurn();
     this.setState({
-      showMe: true,
+      visible: true,
     })
   }
   shouldComponentUpdate(nextProps, nextState) {
-    if(this.state.showMe) {
+    console.log('letter='+this.props.letter, 'winner='+this.props.winner);
+    if((this.props.letter !== '') || this.props.winner !== '') {
       return false;
     }
     else {
       return true;
     }
   }
+  componentDidUpdate() {
+    this.props.checkForWin();
+  }
   render() {
-    let showMe = this.state.showMe;
-    if(showMe) {
+    if(this.props.letter !== '') {
       return (
-        <div className='space'>{this.props.turn === 'x' ? <X/> : <O/>}</div>
+        <div className='space'>{this.props.turn === 'x' ? 'O' : 'X'}</div>
       )
     }
     else {
@@ -96,22 +136,36 @@ class Space extends React.Component {
   }
 }
 
-const X = (props) => {
-  return <span>X</span>;
-}
-
-const O = (props) => {
-  return <span>O</span>;
+const Display = (props) => {
+  return (
+    <div>
+      <h2>{props.winner ? 'Winner:' : 'Next Player:'}</h2>
+      <p>{props.winner ? props.winner : props.turn === 'x' ? 'X' : 'O'}</p>
+      <button onClick={props.reset}>Reset</button>
+    </div>
+  )
 }
 
 ReactDOM.render(<GameBoard/>, document.getElementById('root'));
 
-//Figure out how to update the tracker approprietly and place the winning logic.
-//1 2 3
-//4 5 6
-//7 8 9
-//Winning combinations: 123, 456, 789, 147, 258, 369, 159.
+
+//Figure out how to update the tracker appropriately and place the winning logic.
+//a b c
+//d e f
+//g h i
+//Winning combinations: abc, def, ghi, abg, beh, cfi, aei.
 
 
+/*
+  <Space id={'a'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'b'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'c'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'d'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'e'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'f'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'g'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'h'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+  <Space id={'i'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
+*/
 
 //CONTINUE PROJECT LOGIC
