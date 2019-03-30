d66fd97abd8fdf98bcd875de447b7e3c6a284815
OscarFabiani <spartan539@yahoo.com>
2019-03-30 14:55:31 -0700

    Added 'time travel' code from tutorial.

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class Board extends React.Component {
 
   render() {
     const winner = calculateWinner(this.state.squares);
-    const squareRenders = this.state.squares.map((_, i) =>
+    const squareRenders = Array(9).fill(null).map((_, i) =>
       <Square
         key={i}
         position={i}
@@ -60,11 +60,64 @@ class Board extends React.Component {
   }
 }
 
-
+//I ADDED A BUNCH OF CODE TO THIS COMPONENT BEFORE STOPPING
 class Game extends React.Component {
+  state = {
+    history: [{
+      squares: Array(9).fill(null),
+    }],
+    stepNumber: 0,
+    xIsNext: true,
+  };
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,
+      }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
   render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button key={move} onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     return (
-      <Board />
+      //I COULD REMOVE THE WRAPPER DIV AND OL AND BE BACK TO FUNCTIONAL
+      <div>
+        <Board
+          squares={current.squares}
+          onClick={(i) => this.handleClick(i)}
+        />
+        <ol>
+          {moves}
+        </ol>
+      </div>
     );
   }
 }
