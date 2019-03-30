55cefce0a2b732a64a46545696e6055c1dd6b3f8
OscarFabiani <spartan539@yahoo.com>
2019-03-30 14:21:28 -0700

    Refactored handleClick relationship between Square and Board components.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const Square = ({value, onClick}) => {
+const Square = ({position, value, onClick}) => {
+  //This seems to be a bad practice though it also seems to allow me to forgo the use of
+  //binding in the Board component's render. The reason this is bad practice is because
+  //this component will recreate the handler on every re-render. Maybe I can use useCallback 
+  //which “returns a memoized version of the callback that only changes if one of the inputs
+  //has changed. This is useful when passing callbacks to optimized child components that rely
+  //on reference equality to prevent unnecessary renders.” This 'hook' seems to be included
+  //in create-react-app of of now(03/30/19), but doesn't seem to be working.
+  const handleClick = () => {
+    onClick(position);
+  }
   return (
-    <button className="square" onClick={onClick}>
+    <button className="square" onClick={handleClick}>
       {value}
     </button>
   );
@@ -13,11 +23,11 @@ const Square = ({value, onClick}) => {
 
 class Board extends React.Component {
   state = {
-    squares: Array(9).fill(null),
+    squares: [null, null, null, null, null, null, null, null, null], //Array(9).fill(null),
     xIsNext: true,
   };
-  //Why are these working without binding or method = () syntax?
-  handleClick(i) {
+
+  handleClick = (i) => {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -28,29 +38,28 @@ class Board extends React.Component {
       xIsNext: !this.state.xIsNext,
     })
   }
-  renderSquare(i) {
-    return (
-      <Square
-        key={i}
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    )
-  }
 
   render() {
     const winner = calculateWinner(this.state.squares);
-    const squaresArray = this.state.squares.map((_, i) => this.renderSquare(i));
+    const squareRenders = this.state.squares.map((_, i) =>
+      <Square
+        key={i}
+        position={i}
+        value={this.state.squares[i]}
+        onClick={this.handleClick}
+      />
+    );
     return (
       <div>
         <h3>{winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')}</h3>
         <div className='game-board'>
-          {squaresArray}
+          {squareRenders}
         </div>
       </div>
     );
   }
 }
+
 
 class Game extends React.Component {
   render() {
@@ -86,6 +95,79 @@ for (let i = 0; i < lines.length; i++) {
   }
   return null;
 */
+
+
+
+
+
+//This is the Square and Board components utilizing an arrow function in the rendering of Square components
+//as opposed to binding the handleClick methods and adding a function to the Square component which passes an
+//additional prop passed down from the Board component.
+//I'm not yet sure which method is best in this situation.
+/*
+const Square = ({value, onClick}) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  };
+  //NO BINDING HERE
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    const squareRenders = this.state.squares.map((_, i) =>
+      <Square
+        key={i}
+        place={i}
+        value={this.state.squares[i]}
+        //ARROW FUNCTION USED TO PASS 'i'
+        onClick={() => this.handleClick(i)}
+      />
+    );
+    return (
+      <div>
+        <h3>{winner ? 'Winner: ' + winner : 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')}</h3>
+        <div className='game-board'>
+          {squareRenders}
+        </div>
+      </div>
+    );
+  }
+}
+*/
+
+//This logic was placed within the render method of the Board component and was removed in
+//favor of being implemented as a method of the Board comonent.
+/*
+const squareRenders = this.state.squares.map((_, i) =>
+      <Square
+        key={i}
+        position={i}
+        value={this.state.squares[i]}
+        onClick={this.handleClick}
+      />
+    );
+*/
+
+
 
 
 
