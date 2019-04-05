d2b2f1291a3aeef22ff3fbfef81a02f53c18956c
OscarFabiani <spartan539@yahoo.com>
2019-04-04 17:36:44 -0700

    removed tutorial code prior to 'time travel'.

--- src/index1.js
+++ src/index1.js
@@ -106,48 +106,5 @@ const Display = (props) => {
 ReactDOM.render(<GameBoard/>, document.getElementById('root'));
 
 
-//Figure out how to update the tracker appropriately and place the winning logic.
-//a b c
-//d e f
-//g h i
-//Winning combinations: abc, def, ghi, abg, beh, cfi, aei.
-
-
-//Space components before implementing map
-/*
-  <Space id={'a'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'b'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'c'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'d'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'e'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'f'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'g'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'h'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-  <Space id={'i'} turn={this.state.turn} winner={this.state.winner} updateTracker={this.updateTracker} toggleTurn={this.toggleTurn} checkForWin={this.checkForWin}/>
-*/
-
-//This is the original logic for winning the game before introducing an equals function.
-/*
-if(
-  (a !== '' && ((a === b && a === c) || (a === d && a === g) || (a === e && a === i)))
-    || (c !== '' && ((c === f && c === i) || (c === e && c === g)))
-      || (d !== '' && (d === e && d === f))
-        || (g !== '' && (g === h && g === i))
-          || (b !== '' && (b === e && b === h))) {
-            this.setState({
-              winner: this.state.turn === 'x' ? 'O' : 'X',
-            })
-          }
-
-if (areEqual(a, b, c) || areEqual(a, d, g) || areEqual(a, e, i)
-        || areEqual(c, f, i) || areEqual(c, e, g) || areEqual(d, e, f)
-        || areEqual(g, h, i) || areEqual(b, e, h)) {
-      this.setState({
-        winner: this.state.turn === 'x' ? 'O' : 'X',
-      })
-    }
-const areEqual = (...args) => args.every(v => (v === args[0] && v !== ''));
-if (wins.some(v => areEqual(...v))) {
-*/
 
 //COMPARE WITH TUTORIAL
